6638feb224f00ff28f0f3bb6bbc12134e6e816ee
Yuri Garcia Vaz <yurigava@gmail.com>
2016-05-14 22:26:26 -0300

    Started using openCV and cmake

---
+++ CMakeFiles/windtube.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/windtube.dir/main.cpp.o"
+  "windtube.pdb"
+  "windtube"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/windtube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
